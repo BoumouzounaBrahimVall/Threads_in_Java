0d093b09482bdc12804c519371a9422c96508500
unknown <brahimvall.boumouzouna@etu.fstm.ac.ma>
2022-12-31 14:41:28 +0100

    clean up some methods

--- src/tp4/Jouet.java
+++ src/tp4/Jouet.java
@@ -14,21 +14,8 @@ public class Jouet {
 		nbr = nm;
 	}
 
-	public synchronized void tuEsVerifiePar(VerificateurJouet v) {
-		// traitement faite par Rachid
-		if (v.getNiveau() == 2) {
-			while (!dejaVerifie) {
-				try {v.p.wait();} catch (InterruptedException e1) {}
-			}
-		}
-		// traitment faite par Amine ou Ahmed
-		if (v.getNiveau() == 1 ) {
-			if(dejaVerifie )return;
-			dejaVerifie = true;
-		}
-		
-		// partie 2
-		// Random r=new Random();
+	private void showSleepShow(VerificateurJouet v)
+	{
 		System.out.println("verificateur :" + v.getNom() + " commence a verifier le jouet :" + nbr);
 		try {
 			// partie 2
@@ -38,8 +25,26 @@ public class Jouet {
 		}catch (InterruptedException e) {}
 		
 		System.out.println("    verificateur :" + v.getNom() + " a fini de verifiers le jouet :" + nbr);
-		 if(v.getNiveau() == 1) notifyAll();
+	}
+	public synchronized void tuEsVerifiePar(VerificateurJouet v) {
+		// traitement faite par Rachid
+		if (v.getNiveau() == 2) {
+			while (!dejaVerifie) {
+				try {v.p.wait();} catch (InterruptedException e1) {}
+			}
+				showSleepShow(v);
 
+		}
+		// traitment faite par Amine ou Ahmed
+		if (v.getNiveau() == 1 ) {
+			if(dejaVerifie )return;
+			dejaVerifie = true;
+			showSleepShow(v);
+			notifyAll();
+		}
+		
+		// partie 2
+		// Random r=new Random();
 
 	}
 }
